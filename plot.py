719a3cbbf3f55d44735a9e0390800a93774d4f32
Esh <eshika.k@research.iiit.ac.in>
2021-09-23 01:33:46 +0530

    plot code

--- plot.py
+++ plot.py
@@ -1,21 +1,48 @@
-#id = plot-synopsis-content
-import wikipedia
 import pandas as pd
+import requests # to make TMDB API calls
+from secrets import api_key
 
 df = pd.read_csv('./data/data.csv')
 
-plot = []
+overview = []
+poster = []
+keywords = []
 
 for index, row in df.iterrows():
-    
-    search = row['title'] + ' (' + row['startYear'] + ' film)'
-    try:
-        section = wikipedia.WikipediaPage(search).section('Plot')
-        plot.append(section)
-        # print(section)
-    except:
-        plot.append('')
-    
-df['plot'] = plot
+    url = 'https://api.themoviedb.org/3/movie/' + row['titleId'] + '?api_key=' + api_key + '&external_source=imdb_id'
 
-df.to_csv('./data/data_plots.csv')
+    url_key = 'https://api.themoviedb.org/3/movie/' + row['titleId'] + '/keywords?api_key=' + api_key +'&external_source=imdb_id'
+
+    response = requests.get(url)
+    details = response.json()
+
+    response_key = requests.get(url_key)
+    details_key = response_key.json()
+
+    try:
+        overview.append(details['overview'])
+
+    except:
+        overview.append([])
+        print(details)
+
+    try:
+        poster.append(details['poster_path'])
+    except:
+        poster.append([])
+        print(details)
+
+    try:
+        keywords.append(details_key['keywords'])
+    except:
+        keywords.append([])
+        print(details_key)
+
+    print("done")
+
+df['overview'] = overview
+df['poster'] = poster
+df['keywords'] = keywords
+
+df.to_csv('./data/data_new.csv')
+# https://image.tmdb.org/t/p/original/ + path 
