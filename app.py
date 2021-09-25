43214796018c293faaed5e212735c081dffece80
Keshav Bajaj <keshav.bajaj@research.iiit.ac.in>
2021-09-26 04:08:07 +0530

    Done with no CSS

--- app.py
+++ app.py
@@ -1,51 +1,59 @@
 import pickle
-from PyScripts.content_based import conditions
-from flask import Flask, render_template, request
-import sys
-sys.path.append('/PyScripts/')
+from PyScripts.content_based import conditions, final_rec
+from flask import Flask, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    genres = []
-
-    with open('data/genres.pkl', 'rb') as f:
-        genres = pickle.load(f)
-
-    return render_template('index.html',
-                           genres=genres)
+# Check Configuration section for more details
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config.from_object(__name__)
+Session(app)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def search():
-    genre = request.form.get('genre-select')
-    languages = request.form.getlist('languages')
+    if request.method == 'POST':
 
-    df = (conditions(([genre]), 2000, 2020, languages))
-    options = df.head(250).sample(n=10).to_dict('records')
+        session["genre"] = request.form.get(
+            'genre-select') if request.form.get('genre-select') else session["genre"]
+        session["languages"] = request.form.getlist('languages') if len(
+            request.form.getlist('languages')) else session["languages"]
+        session["years"] = request.form.getlist('years') if len(
+            request.form.getlist('years')) else session["years"]
 
-    return render_template('options.html', options=options)
+        if session["years"][0] > session["years"][1]:
+            return render_template('index.html',
+                                   genres=session["genres"], error=1)
+
+        df = (conditions(([session["genre"]]), int(session["years"][0]),
+              int(session["years"][1]), session["languages"]))
+        l = len(df)
+        options = df.head(min(250, l)).sample(n=min(10, l)).to_dict('records')
+
+        return render_template('options.html', options=options)
+
+    session["genres"] = []
+    session["index"] = 0
+    with open('data/genres.pkl', 'rb') as f:
+        session["genres"] = pickle.load(f)
+
+    return render_template('index.html',
+                           genres=session["genres"], error=0)
 
 
-@app.route("/suggestion", methods=['GET', 'POST'])
+@app.route("/suggestion", methods=['POST'])
 def get():
-    movies = request.form.getlist('movies')
+    if request.method == 'POST':
+        session["movies"] = request.form.getlist('movies') if len(
+            request.form.getlist('movies')) else session["movies"]
 
-    # l = (len(df))
+        final_movie = final_rec([session["genre"]], session["languages"], session["movies"], int(
+            session["years"][0]), int(session["years"][1]))
 
-    # print(df.head(250).sample(n=10))
+        session["index"] += 1
 
-    # df = df.head(min(l, 10000))
-    # df['soup'] = df['soup'] + ' ' + df['overview']
-    # sim, indice, titles = fit_and_transform_soup(df)
-
-    # rec = get_recommendations(
-    #     'Harry Potter and the Deathly Hallows: Part 2', sim, indice, titles)
-    # print(movies)
-
-    return 'hey'
+        return render_template('final.html', movie=final_movie.iloc[session["index"]])
 
 
 if __name__ == '__main__':
